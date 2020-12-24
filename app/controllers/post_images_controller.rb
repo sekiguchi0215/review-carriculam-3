08501829d81cdfa6e1c23b7582066e55bfd62c21
vagrant <dsphere5296@gmail.com>
2020-12-24 09:10:22 +0000

    [Finish] App-Complete2 chapter3

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,31 @@
+class PostImagesController < ApplicationController
+	def index
+		@post_images = PostImage.all
+	end
+
+	def new
+		@post_image = PostImage.new
+	end
+
+	def create
+		@post_image = PostImage.new(post_image_params)
+		@post_image.user_id = current_user.id
+		@post_image.save
+		redirect_to post_images_path
+		# if @post_image.save
+		# 	redirect_to post_image_path
+		# else
+		# 	render "new"
+		# end
+	end
+
+	def show
+		@post_image = PostImage.find(params[:id])
+	end
+
+	private
+
+	def post_image_params
+		params.require(:post_image).permit(:shop_name, :image, :caption)
+	end
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '968ef2df925492d7ed1dfbe11e8de13a28d8dfd5e281c8e3f581a426ddb96d7ef84dab3cd089bd30a1975d5b760ad2a6fe6744b5bcf3c43703336e07225a4558'
